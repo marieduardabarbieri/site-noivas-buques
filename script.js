71e724f58617c4d94a736a4563fee53f4e47fefc
Maylla Nascimento <maylla.rie@gmail.com>
2025-10-06 15:51:09 -0300

    timer JavaScript

--- script.js
+++ script.js
@@ -55,3 +55,27 @@ window.addEventListener("resize", () => {
 
 updateDots();
 updateCarousel();
+
+
+  const countdownDate = new Date("Dec 31, 2025 23:59:59").getTime();
+
+  const timer = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    if (distance < 0) {
+      clearInterval(timer);
+      document.querySelector(".deal-timer").innerHTML = "<p>Promoção Expirada</p>";
+    }
+  }, 1000);
+
